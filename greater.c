708ac1b97aa85e703187a6ddf4e9f1f308d59e54
kunalhacks <114162471+kunalhacks@users.noreply.github.com>
2022-11-14 11:15:37 +0530

    greater.c
    Program to find out the Greater one from the given integers using C.

---
+++ greater.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int a,b,c;
+
+	printf("Enter three integers\n");
+	scanf("%d %d %d", &a,&b,&c);
+
+	if (a>b && a>c)
+		printf("A is Greater");
+
+	else if("b>c")
+		printf("B is Greater");
+
+	else
+		printf("C is Greater");
+
+	getch();
+}
